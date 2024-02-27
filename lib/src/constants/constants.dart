84544979a1cce7713c5cc3e812c252fd83139843
Laurence Troy Sambaa Valdez <laurencetroyv@gmail.com>
2024-02-27 22:20:49 +0800

    ✨ feat (page): Add constants and tab bar navigation

---
+++ lib/src/constants/constants.dart
@@ -0,0 +1,8 @@
+const kAppName = "Liceo Campus Walk";
+
+const kMon = "Mon";
+const kTue = "Tue";
+const kWed = "Wed";
+const kThu = "Thu";
+const kFri = "Fri";
+const kSat = "Sat";
---
+++ lib/src/pages/tab_bar_navigation.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:ldcu/src/constants/constants.dart';
+import 'package:ldcu/src/pages/visual_map.dart';
+
+import 'add_section.dart';
+import 'settings.dart';
+
+class TabBarNavigation extends ConsumerWidget {
+  const TabBarNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SafeArea(
+      child: DefaultTabController(
+        length: 6,
+        initialIndex: 0,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(kAppName),
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: kMon),
+                Tab(text: kTue),
+                Tab(text: kWed),
+                Tab(text: kThu),
+                Tab(text: kFri),
+                Tab(text: kSat),
+              ],
+            ),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const AddSection()),
+                  );
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.settings),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Settings()),
+                  );
+                },
+              ),
+            ],
+          ),
+          body: const TabBarView(
+            children: [
+              Text(kMon),
+              Text(kTue),
+              Text(kWed),
+              Text(kThu),
+              Text(kFri),
+              Text(kSat),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const VisualMap(),
+                ),
+              );
+            },
+            child: const Icon(Icons.map),
+          ),
+        ),
+      ),
+    );
+  }
+}
