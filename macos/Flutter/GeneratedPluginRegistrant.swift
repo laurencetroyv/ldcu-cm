16ad891764759904ecee9abd083b7d88a62dcb10
Laurence Troy Sambaa Valdez <laurencetroyv@gmail.com>
2024-02-27 22:19:43 +0800

    ➕ build (deps): Update dependencies in pubspec.yaml
    Add flutter_local_notifications v16.3.2
    Add flutter_riverpod v2.4.10
    Add gap v3.0.1
    Add flutter_svg v2.0.10+1
    Add geolocatior v11.0.0
    Add intl v0.19.0
    Add isarv3.1.0+1
    Add isar_flutter_libs 3.1.0+1
    Add just_audio v0.9.36
    Add o3d v3.1.0
    Add path_provider v2.1.2
    
    build dependencies
    Add build_runner v2.4.8
    Add isar_generator v3.1.0+1

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
+import flutter_local_notifications
+import geolocator_apple
+import isar_flutter_libs
+import just_audio
+import path_provider_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
