93782475cbabfd3111ce09b7767714b6da106e4d
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-03-02 15:06:55 +0800

    ♻️ refactor (page): Remove search functionality from VisualMap page

--- lib/src/pages/visual_map.dart
+++ lib/src/pages/visual_map.dart
@@ -12,50 +12,11 @@ class VisualMap extends ConsumerStatefulWidget {
 
 class _VisualMapState extends ConsumerState<VisualMap> {
   final _o3d = O3DController();
-  final _searchController = SearchController();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: SearchAnchor(
-            searchController: _searchController,
-            headerTextStyle: const TextStyle(
-                color: Colors.white, fontWeight: FontWeight.w500),
-            isFullScreen: false,
-            viewBackgroundColor: const Color(0xFF8E869C),
-            builder: (context, controller) {
-              return SearchBar(
-                leading: const Icon(Icons.search, color: Color(0xFF8E869C)),
-                padding: const MaterialStatePropertyAll(
-                  EdgeInsets.symmetric(horizontal: 16.0),
-                ),
-                controller: controller,
-                onTap: () => controller.openView(),
-                textStyle: const MaterialStatePropertyAll(
-                  TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
-                  ),
-                ),
-                shape: const MaterialStatePropertyAll(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(16)),
-                    side: BorderSide.none,
-                  ),
-                ),
-                constraints: const BoxConstraints(
-                  minHeight: 48,
-                  maxHeight: 48,
-                ),
-              );
-            },
-            suggestionsBuilder: (context, controller) {
-              return [];
-            },
-          ),
-        ),
         body: O3D.asset(
           touchAction: TouchAction.none,
           controller: _o3d,
