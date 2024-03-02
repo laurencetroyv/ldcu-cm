26711df38863263f12e1ac999c494561a7883c33
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-03-02 15:03:48 +0800

    ✨ feat (page): Add theme and map switch functionality to Settings page

--- lib/src/pages/settings.dart
+++ lib/src/pages/settings.dart
@@ -2,21 +2,43 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:ldcu/src/provider/switch_provider.dart';
+import 'package:ldcu/src/provider/theme_provider.dart';
+
 class Settings extends ConsumerWidget {
   const Settings({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    bool isMap3D = ref.watch(switchMapProvider);
+    bool isDark = ref.watch(themeProvider);
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Settings'),
         ),
-        body: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Center(
-            child: Text("Coming soon"),
-          ),
+        body: ListView(
+          children: [
+            ListTile(
+              title: Text(
+                  'Switch Theme to ${isDark ? 'Light Mode' : 'Dark Mode'}'),
+              trailing: Switch(
+                value: ref.watch(themeProvider),
+                onChanged: (value) {
+                  ref.read(themeProvider.notifier).switchTheme(value);
+                },
+              ),
+            ),
+            ListTile(
+              title: Text('Switch Map to ${isMap3D ? '2D' : '3D'}'),
+              trailing: Switch(
+                value: ref.watch(switchMapProvider),
+                onChanged: (value) {
+                  ref.read(switchMapProvider.notifier).switchMap(value);
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
