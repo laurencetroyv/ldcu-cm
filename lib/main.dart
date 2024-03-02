a607f5c6721889463278547b3a317c25a619ed23
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-03-02 15:04:31 +0800

    ✨ feat (main): Add theme support to MainApp and wrap MainApp with ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:ldcu/src/pages/tab_bar_navigation.dart';
+import 'package:ldcu/src/provider/theme_provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    bool isDark = ref.watch(themeProvider);
+
     return MaterialApp(
       home: const TabBarNavigation(),
       debugShowCheckedModeBanner: false,
+      themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
       theme: ThemeData.light().copyWith(
         colorScheme: ThemeData.light().colorScheme.copyWith(
+              primary: const Color(0xFF89201a),
+              secondary: const Color(0xFF89201a),
+            ),
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: ThemeData.dark().colorScheme.copyWith(
               primary: const Color(0xFF89201a),
               secondary: const Color(0xFF89201a),
             ),
