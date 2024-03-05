c3e866fb3e2f35cb2d1a089d1a9fa44e89d1e009
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-03-05 12:51:14 +0800

    ✨ feat (page): Update map source in VisualMap widget

--- lib/src/pages/visual_map.dart
+++ lib/src/pages/visual_map.dart
@@ -4,8 +4,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:o3d/o3d.dart';
 
 class VisualMap extends ConsumerStatefulWidget {
-  const VisualMap(this.name, {super.key});
-  final String name;
+  const VisualMap(this.mapSrc, {super.key});
+  final String mapSrc;
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => _VisualMapState();
@@ -18,12 +18,21 @@ class _VisualMapState extends ConsumerState<VisualMap> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: O3D.asset(
-          touchAction: TouchAction.none,
-          controller: _o3d,
-          src: "assets/visual_map.gltf",
-        ),
-      ),
+          body: O3D.asset(
+            touchAction: TouchAction.none,
+            controller: _o3d,
+            src: widget.mapSrc,
+          ),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: const Color(0xFF89201a),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+          )),
     );
   }
 }
